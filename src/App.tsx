32c23e6b953586abd44097754296fbe5ee20c39b
MutazHusam <m.mutaz.m@gmail.com>
2022-08-24 12:42:06 +0300

    Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import "./App.css";
 import Bag from "./components/Bag/Bag";
-import Home from './screens/Home';
-import { Login } from './screens/Login';
-import { SignUp } from './screens/SignUp';
-import { Product } from './screens/Product';
-
+import Home from "./screens/Home";
+import { Login } from "./screens/Login";
+import { SignUp } from "./screens/SignUp";
+import { Product } from "./screens/Product";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    // <Home/>
-    // <SignUp/>
-    // <Login/>
-    // <Bag />
-    <Product/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Product" element={<Product />} />
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Bag" element={<Bag />} />
+    </Routes>
   );
 }
 
